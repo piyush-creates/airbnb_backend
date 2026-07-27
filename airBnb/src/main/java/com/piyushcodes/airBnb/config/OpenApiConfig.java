0a54b5deae8c14acef8ad2a14ef1e3bb0ca47cc4
Piyush Kumar <vpiyush318@gmail.com>
2026-07-27 18:34:54 +0530

    Deployment Ready

---
+++ airBnb/src/main/java/com/piyushcodes/airBnb/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package com.piyushcodes.airBnb.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        security = {
+                @SecurityRequirement(name = "Bearer Authentication")
+        }
+)
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+
+}
